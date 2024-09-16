b49c5f236167472ce78463142c9eda08fd5e856c
Kim Jansson <kumpe82@gmail.com>
2024-09-16 19:10:33 +0200

    error handling

--- README.md
+++ README.md
@@ -17,9 +17,9 @@ PoC module for automating customization pipeline invokation in Account Factory f
 
 | Name | Version |
 |------|---------|
-| <a name="provider_aws"></a> [aws](#provider\_aws) | >= 5.11.0, < 6.0.0 |
-| <a name="provider_local"></a> [local](#provider\_local) | n/a |
-| <a name="provider_random"></a> [random](#provider\_random) | n/a |
+| <a name="provider_aws"></a> [aws](#provider\_aws) | 5.67.0 |
+| <a name="provider_local"></a> [local](#provider\_local) | 2.5.2 |
+| <a name="provider_random"></a> [random](#provider\_random) | 3.6.3 |
 
 ## Resources
 
--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "codestar_connection_arn" {
   description = "ARN of the CodeStar connection"
-  value       = aws_codestarconnections_connection.github.arn
+  value       = aws_codestarconnections_connection.github[0].arn
 }
 
 output "codepipeline_bucket_name" {
   description = "Name of the S3 bucket to store the CodePipeline artifacts"
-  value       = aws_s3_bucket.codepipeline_bucket.bucket
+  value       = aws_s3_bucket.codepipeline_bucket[0].bucket
 }
 
 output "codepipeline_bucket_arn" {
   description = "ARN of the S3 bucket to store the CodePipeline artifacts"
-  value       = aws_s3_bucket.codepipeline_bucket.arn
+  value       = aws_s3_bucket.codepipeline_bucket[0].arn
 }
 
 output "invokation_kms_key_arn" {
   description = "ARN of the KMS key used to encrypt the CodePipeline artifacts"
-  value       = aws_kms_key.invoke.arn
+  value       = aws_kms_key.invoke[0].arn
 }
 
 output "codepipeline_arn" {
